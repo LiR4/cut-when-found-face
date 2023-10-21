79e52e539b75637e9c0aafde2bff22b5877adedc
Pedro Henrique Lira Da Silva Bispo <83731045+LiR4@users.noreply.github.com>
2023-10-21 13:15:06 -0300

    feat: add cut video on time when found face

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
 # https://stackoverflow.com/questions/33650974/opencv-python-read-specific-frame-using-videocapture
 # https://stackoverflow.com/questions/24361771/frame-rate-of-video-python
 
-
 import cv2
 import numpy as np
-      
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-video = cv2.VideoCapture('20231014_094039.mp4')
+def cut(video_file, time_start, time_end, count, second):
+    if(second == 0):
+        second = 1
+        
+    ffmpeg_extract_subclip(video_file, time_start, time_end+second, targetname=f"./data/clip/cut{count}.mp4")
+
+video_file = "data/20231014_094039.mp4"
+
+video = cv2.VideoCapture(video_file)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 count = 0
 
+width= video.get(cv2.CAP_PROP_FRAME_WIDTH)
+height= video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+last_time = None
 time_start = None
 time_end = None
+seconds = 0
 
 while True:
     sucess, img = video.read()
     faces = face_cascade.detectMultiScale(img, 1.3, 4)
-    for(x,y,w,h) in faces:
-        ROI = img[y:y+h, x:x+w]
-    
-        blur = cv2.GaussianBlur(ROI, (99, 99), 0)
-        img[y:y+h, x:x+w] = blur
         
-    
-    if(type(faces).__module__ == np.__name__ and count == 0):
+    if(type(faces).__module__ == np.__name__):
         time_start = (video.get(cv2.CAP_PROP_POS_MSEC))/1000
+        last_time = time_start
         
     if(type(faces).__module__ != np.__name__):
         time_end = (video.get(cv2.CAP_PROP_POS_MSEC))/1000
     
-    #if(time_start != 0 and time_end != 0): # cut section
+    if(time_start != 0 and time_end != 0): # cut section
+        count += 1
         
-
-    print(f"time start: {time_start}, time end: {time_end}")
+        if(last_time == time_start):
+            seconds +=1
+        else:
+            seconds = 0
+       
+        cut(video_file, time_start, time_end, count, seconds)
         
+        print(f"time start: {time_start}, time end: {time_end+1}")
+        
+        time_start = 0
+        time_end = 0   
     
-      
-   
-    
-#   cv2.imshow("teste",img)
+    cv2.imshow("teste",img)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
