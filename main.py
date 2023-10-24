e5bb04503529f754c316fff55a76d6db2278d2b4
LiR4 <pedrolira521@gmail.com>
2023-10-24 11:42:11 -0300

    refactor: variables and def cut

--- main.py
+++ main.py
@@ -6,23 +6,20 @@ import numpy as np
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 def cut(video_file, time_start, time_end, count, second):
-    if(second == 0):
-        second = 1
-        
     ffmpeg_extract_subclip(video_file, time_start, time_end+second, targetname=f"./data/clip/cut{count}.mp4")
 
 video_file = "data/20231014_094039.mp4"
 
 video = cv2.VideoCapture(video_file)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-count = 0
 
 width= video.get(cv2.CAP_PROP_FRAME_WIDTH)
 height= video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
+count = 0
 last_time = None
-time_start = None
-time_end = None
+time_start = 0.0
+time_end = 0.0
 seconds = 0
 
 while True:
@@ -36,22 +33,20 @@ while True:
     if(type(faces).__module__ != np.__name__):
         time_end = (video.get(cv2.CAP_PROP_POS_MSEC))/1000
     
-    if(time_start != 0 and time_end != 0): # cut section
+    if(time_start != 0.0 and time_end != 0.0): # cut section
         count += 1
-        
         if(last_time == time_start):
             seconds +=1
         else:
-            seconds = 0
-       
+            seconds = 1
+        
         cut(video_file, time_start, time_end, count, seconds)
-        
-        print(f"time start: {time_start}, time end: {time_end+1}")
-        
-        time_start = 0
-        time_end = 0   
+        print(f"time start: {time_start}, time end: {time_end+seconds}")
+
+        time_start = 0.0
+        time_end = 0.0
     
-    cv2.imshow("teste",img)
+    #cv2.imshow("teste",img)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
