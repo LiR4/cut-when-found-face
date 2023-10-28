7807633c9c5e08abb9a2725011dfb481d8696929
Pedro Henrique Lira Da Silva Bispo <83731045+LiR4@users.noreply.github.com>
2023-10-28 11:27:23 -0300

    fix: reduced qtd of clips but not accurate in time

--- main.py
+++ main.py
@@ -3,24 +3,26 @@
 
 import cv2
 import numpy as np
+import os
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 def cut(video_file, time_start, time_end, count, second):
     ffmpeg_extract_subclip(video_file, time_start, time_end+second, targetname=f"./data/clip/cut{count}.mp4")
+
+if(os.path.isdir('./data/clip') == False):
+    clip = os.path.join('data', 'clip')
+    os.makedirs(clip)
 
 video_file = "data/20231014_094039.mp4"
 
 video = cv2.VideoCapture(video_file)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-width= video.get(cv2.CAP_PROP_FRAME_WIDTH)
-height= video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
 count = 0
-last_time = None
+last_time = []
 time_start = 0.0
 time_end = 0.0
-seconds = 0
+seconds = 3
 
 while True:
     sucess, img = video.read()
@@ -28,26 +30,22 @@ while True:
         
     if(type(faces).__module__ == np.__name__):
         time_start = (video.get(cv2.CAP_PROP_POS_MSEC))/1000
-        last_time = time_start
+        last_time.append(time_start)
         
     if(type(faces).__module__ != np.__name__):
         time_end = (video.get(cv2.CAP_PROP_POS_MSEC))/1000
     
     if(time_start != 0.0 and time_end != 0.0): # cut section
-        count += 1
-        if(last_time == time_start):
-            seconds +=1
-        else:
-            seconds = 1
-        
-        cut(video_file, time_start, time_end, count, seconds)
-        print(f"time start: {time_start}, time end: {time_end+seconds}")
+        if(last_time[count]+1 < time_start):
+            cut(video_file, time_start, time_end, count, seconds)
+            print(f"time start: {time_start}, time end: {time_end+seconds}")
+            count += 1
 
         time_start = 0.0
         time_end = 0.0
     
-    #cv2.imshow("teste",img)
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    # cv2.imshow("teste",img)
+    if sucess != True:
         break
 
 video.release()
